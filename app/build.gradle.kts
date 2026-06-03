fee68bb05d9f2997f2eaa564025e76e05955590b
Serhii Tymoshenko <serhij.tymoschenko@gmail.com>
2026-06-03 21:16:43 +0200

    Updated version code

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,8 +36,8 @@ configure<ApplicationExtension> {
         applicationId = "com.mashiverse.mashit"
         minSdk = 28
         targetSdk = 37
-        versionCode = 35
-        versionName = "18.05.2026"
+        versionCode = 36
+        versionName = "03.06.2026"
 
         val localProperties = Properties()
         val localPropertiesFile = File(rootDir, "keys.properties")
